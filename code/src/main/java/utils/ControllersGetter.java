cd6703b10780f202373d277db734a9e5ed5d81c6
= <AYOUBMAJID71@GMAIL.COM>
2025-01-12 11:51:22 +0100

    prepare to merge

--- code/src/main/java/utils/ControllersGetter.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-import controller.AccountsController;
-import model.Accounts.AccountToken;
-
-public class ControllersGetter {
-     public static AccountsController accountsController = new AccountsController();
-     public static AccountToken currentAccountSession=null;
-}
